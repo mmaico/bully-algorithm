fa9adbbf699955e522db52a223e085999d160b22
Marcelo Maico <mmaico@gmail.com>
2017-05-25 21:56:39 -0300

    Refactory in packages.

--- src/main/java/bully/domain/service/CandidatesScoreGreaterService.java
+++ src/main/java/bully/domain/service/CandidatesScoreGreaterService.java
@@ -1,6 +1,7 @@
-package bully.domain.service.language;
+package bully.domain.service;
 
 import bully.domain.model.comunication.Responses;
+import bully.domain.service.language.MyScore;
 
 @FunctionalInterface
 public interface CandidatesScoreGreaterService {
